e52463fd050518ae1e82b097074deaf8b37b2ae8
Misk-I-ntegration-Technology <devbasmala.ahmed@gmail.com>
2025-08-19 03:20:46 +0300

    Setup Project

---
+++ raise-right-assessment/src/app/core/services/campaign.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { Campaign } from '../../shared/models/campaign.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CampaignService {
+private REST_API = 'https://raise-right-assessment-mocks.up.railway.app/api/campaigns';
+  constructor(private http: HttpClient, private apollo: Apollo) { }
+
+// Fetches a list of all campaigns from the REST API.
+  getCampaigns(): Observable<Campaign[]> {
+    return this.http.get<Campaign[]>(this.REST_API);
+  }
+
+  /**
+   * Fetches detailed information about a specific campaign using GraphQL.
+   * @param id The unique identifier of the campaign to retrieve.
+   * @returns An Observable emitting the campaign's details, including donor information.
+   */
+  getCampaignDetails(id: number): Observable<any> {
+    return this.apollo.watchQuery({
+      query: gql`
+        query getCampaignDetails($id: ID!) {
+          campaign(id: $id) {
+            id
+            name
+            goal
+            currentAmount
+            description
+            imageUrl
+            donors {
+              name
+              amount
+            }
+          }
+        }
+      `,
+      variables: { id }
+    }).valueChanges;
+  }
+}
---
+++ raise-right-assessment/src/app/features/Campaigns/campaigns-list/campaigns-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-campaigns-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './campaigns-list.component.html',
+  styleUrl: './campaigns-list.component.scss'
+})
+export class CampaignsListComponent {
+
+}
